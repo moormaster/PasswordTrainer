6c31c22819373720332984585fee877637e68a66
moormaster <moormaster@gmx.net>
2016-04-28 18:58:38 +0200

    added salt generator component

---
+++ public_html/js/SaltGenerator.js
@@ -0,0 +1,34 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var SaltGenerator = function(length, charSet) {
+    this.prototype = new ISaltGenerator(length, charSet);
+    
+    this.length = length;   // the desired length of the salt
+    this.charSet = charSet;  // the desired charset
+
+    if (!this.charSet)
+        this.charSet = '!"§$%&/()=?`´+*#\'~0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    
+    /*
+     * Generates and returns a salt value (as specified by the constructor 
+     * parameters)
+     */
+    this.generate = function() {
+        var salt = "";
+
+        if (!charSet)
+            charSet = '!"§$%&/()=?`´+*#\'~0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+        for (var i=0;i<length;i++) {
+            var rnd = Math.floor(Math.random()*charSet.length);
+
+            salt += charSet[rnd]
+        }
+
+        return salt;
+    };
+}
