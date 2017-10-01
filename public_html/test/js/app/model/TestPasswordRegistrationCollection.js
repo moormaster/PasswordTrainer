ffbd9d0fdc5fb1d9c5964e46fc3a2494fe5298f3
moormaster <moormaster@gmx.net>
2017-10-01 19:00:53 +0200

    removed netbeans default license header

--- public_html/test/js/app/model/TestPasswordRegistrationCollection.js
+++ public_html/test/js/app/model/TestPasswordRegistrationCollection.js
@@ -1,9 +1,3 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /*
  * PasswordHasherMock with identity function as hash function and rudimental salting
  */
--- public_html/test/js/app/ui/TestLeveledScoreFormatter.js
+++ public_html/test/js/app/ui/TestLeveledScoreFormatter.js
@@ -1,9 +1,3 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 QUnit.module("LeveledScoreFormatter", {
     beforeEach: function() {
         this.formatter = new LeveledScoreFormatter();
