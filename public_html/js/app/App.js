2ed6064bbf28056164f90d4796c13d448a5e88cf
moormaster <moormaster@gmx.net>
2017-10-01 19:05:36 +0200

    fixed: App.js:App.ImportJSON() crashes when jQuery variable $ is not available

--- public_html/js/app/App.js
+++ public_html/js/app/App.js
@@ -39,7 +39,8 @@ var App = (
                     this.writePasswordRegistrationsToLocalStorage();
                     
                     this.pageTrainPasswords.update();
-                    $.mobile.changePage('#pageTrainPasswords');
+                    if ($)
+                        $.mobile.changePage('#pageTrainPasswords');
                 }
             }
 
