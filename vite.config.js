68af1b4696377567161ae5a8320d8fc5ea0ac706
moormaster <moormaster@gmx.net>
2023-04-10 11:15:00 +0200

    add generation of self signed certificate when calling 'npm run preview -- --https'

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vuetify(), vueDevTools()],
+  plugins: [basicSsl(), vue(), vuetify(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
