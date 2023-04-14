9a842cdcc7b6e595ac2ec56ce57fab3a0404333b
moormaster <moormaster@gmx.net>
2023-04-14 23:19:22 +0200

    attempt to create vue-js component for JQPasswordInput

---
+++ src/js/app/ui/PasswordInputComponent.vue
@@ -0,0 +1,44 @@
+// vi: ts=2 et
+
+<script>
+export default {
+  data() {
+    return {
+      color: null, // "success" | "failure" | null
+      description: '',
+      isReadonly: false,
+      status: '',
+    }
+  },
+}
+</script>
+
+<template>
+  <h2 class="passworddescription">{{ description }}</h2>
+  <div>
+    <input type="password" class="password" data-role="none" :readonly="isReadonly" /><br />
+    <span class="passwordscore">{{ status }}</span>
+  </div>
+</template>
+
+<style scoped>
+.bg_anim_red {
+  transition: all 1s ease-in-out;
+  background-color: red;
+}
+
+.bg_anim_green {
+  transition: all 1s ease-in-out;
+  background-color: green;
+}
+
+.bg_anim_yellow {
+  transition: all 1s ease-in-out;
+  background-color: yellow;
+}
+
+.bg_readonly {
+  transition: all 1s ease-in-out;
+  background-color: grey;
+}
+</style>
--- src/js/init.mjs
+++ src/js/init.mjs
@@ -1,5 +1,16 @@
 // vi: ts=2 et
 
+// import { createApp } from 'vue'
+// import App from './App.vue'
+//
+// // Vuetify
+// import 'vuetify/styles'
+// import { createVuetify } 'vuetify'
+//
+// const vuetify = createVuetify()
+//
+// create(App).use(vuetify).mount('#app')
+
 import { App } from './app/App.mjs'
 ;(function ($) {
   var app
