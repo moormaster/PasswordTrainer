8b58b32ce49ba8d26f27f4a0d1d54cb5543585ed
Andre Herbst <moormaster@gmx.net>
2024-04-07 20:39:54 +0200

    refactor: convert PageManagePasswords into vue component

---
+++ src/components/PageManagePasswords.vue
@@ -0,0 +1,126 @@
+// vi: ts=2 et
+
+<script>
+import { LeveledScore } from '../js/app/model/LeveledScore.mjs'
+import { LeveledScoreFormatter } from '../js/app/ui/LeveledScoreFormatter.mjs'
+import PasswordDialog from './PasswordDialog.vue'
+
+export default {
+  components: {
+    PasswordDialog,
+  },
+  data() {
+    return {
+      dialogVisible: false,
+      dialogDescription: '',
+    }
+  },
+  inject: ['appInstance'],
+  methods: {
+    formatPasswordLevel(passwordRegistration) {
+      return new LeveledScoreFormatter().formatLevel(
+        new LeveledScore(passwordRegistration.scoreData),
+      )
+    },
+
+    formatPasswordScore(passwordRegistration) {
+      return new LeveledScoreFormatter().formatScore(
+        new LeveledScore(passwordRegistration.scoreData),
+      )
+    },
+
+    formatPasswordStatus(passwordRegistration) {
+      return new LeveledScoreFormatter().formatStatus(
+        new LeveledScore(passwordRegistration.scoreData),
+      )
+    },
+
+    getLockHours(passwordRegistration) {
+      return new LeveledScore(passwordRegistration.scoreData).lockHours
+    },
+
+    getLockHoursLeft(passwordRegistration) {
+      return new LeveledScore(passwordRegistration.scoreData).lockHoursLeft
+    },
+
+    getPasswordRegistrations() {
+      let passwordRegistrations = []
+
+      let registrations = this.appInstance.applicationModel.passwordRegistrations.getAll()
+      for (let key in registrations) passwordRegistrations.push(registrations[key])
+
+      return passwordRegistrations
+    },
+
+    onAddOrSavePasswordRegistration(description, password) {
+      this.appInstance.addPasswordRegistration(description, password)
+
+      this.dialogVisible = false
+      this.dialogDescription = ''
+    },
+  },
+}
+</script>
+
+<template>
+  <table class="passwordTable">
+    <thead>
+      <tr>
+        <th>Description</th>
+        <th>Score / Level</th>
+        <th>Status</th>
+      </tr>
+    </thead>
+
+    <tbody>
+      <tr
+        class="password"
+        v-for="passwordRegistration in getPasswordRegistrations()"
+        :key="passwordRegistration.description"
+      >
+        <td class="passworddescription">{{ passwordRegistration.description }}</td>
+        <td>
+          <span class="passwordscore">{{ formatPasswordScore(passwordRegistration) }}</span> /
+          <span class="passwordlevel">{{ formatPasswordLevel(passwordRegistration) }}</span>
+        </td>
+        <td style="background-color: lightgrey; padding-top: 3px; padding-bottom: 3px">
+          <span
+            class="passwordlockprogressbar passwordstatus"
+            :style="{
+              backgroundColor: 'white',
+              display: 'block',
+              whiteSpace: 'nowrap',
+              width:
+                100 *
+                  (1 -
+                    this.getLockHoursLeft(passwordRegistration) /
+                      this.getLockHours(passwordRegistration)) +
+                '%',
+            }"
+          >
+            {{ formatPasswordStatus(passwordRegistration) }}
+          </span>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+
+  <passwordDialog
+    v-model:dialogVisible="dialogVisible"
+    v-model:description="dialogDescription"
+    @onAddOrSave="
+      (dialogData) => onAddOrSavePasswordRegistration(dialogData.description, dialogData.password)
+    "
+  >
+  </passwordDialog>
+
+  <div data-role="footer" data-position="fixed">
+    <div data-role="navbar">
+      <ul>
+        <li><a href="#pageTrainPasswords">Train</a></li>
+        <li><a href="#pageManagePasswords" class="ui-btn-active ui-state-persist">Manage</a></li>
+        <li><a href="#pageImportExport">Import/Export</a></li>
+      </ul>
+    </div>
+  </div>
+</template>
---
+++ src/components/PasswordDialog.vue
@@ -0,0 +1,106 @@
+// vi: ts=2 et
+
+<script>
+export default {
+  props: {
+    dialogVisible: Boolean,
+    description: String,
+  },
+  emits: ['update:dialogVisible', 'update:description', 'onAddOrSave'],
+  data() {
+    return {
+      descriptionRules: [
+        (value) => {
+          if (value?.length > 0) return true
+
+          return 'You must enter a description!'
+        },
+      ],
+      password: '',
+      passwordRepeat: '',
+      passwordRules: [
+        () => {
+          if (this.password == this.passwordRepeat) return true
+
+          return 'Passwords must match!'
+        },
+      ],
+    }
+  },
+  methods: {
+    clear() {
+      this.password = ''
+      this.passwordRepeat = ''
+    },
+    async onSubmit(event) {
+      let results = await event
+
+      if (!results.valid) return
+
+      this.$emit('onAddOrSave', {
+        description: this.description,
+        password: this.password,
+      })
+
+      this.clear()
+    },
+  },
+}
+</script>
+
+<template>
+  <v-dialog
+    :model-value="dialogVisible"
+    @update:model-value="(value) => $emit('update:dialogVisible', value)"
+  >
+    <template v-slot:activator="{ props: activatorProps }">
+      <v-btn color="primary" dark v-bind="activatorProps">Add/Edit password</v-btn>
+    </template>
+    <v-form validate-on="submit lazy" @submit.prevent="onSubmit">
+      <v-card>
+        <v-card-title>
+          <span class="headline">Add/Edit Password</span>
+        </v-card-title>
+        <v-card-text>
+          <v-row no-gutters>
+            <v-text-field
+              label="Description*:"
+              :rules="descriptionRules"
+              :value="description"
+              @input="$emit('update:description', $event.target.value)"
+            >
+            </v-text-field>
+          </v-row>
+          <v-row>
+            <v-col>
+              <v-text-field v-model="password" label="Password*" @input="passwordRepeat = ''">
+              </v-text-field>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col>
+              <v-text-field
+                v-model="passwordRepeat"
+                label="Password repeat*"
+                :rules="passwordRules"
+              >
+              </v-text-field>
+            </v-col>
+          </v-row>
+          <small>*indicates required field</small>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn
+            tabindex="1"
+            color="blue darken-1"
+            flat
+            @click="$emit('update:dialogVisible', false)"
+            >Abort</v-btn
+          >
+          <v-spacer></v-spacer>
+          <v-btn tabindex="0" color="blue darken-1" flat type="submit">Add/Save</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-form>
+  </v-dialog>
+</template>
